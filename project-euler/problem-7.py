df33b9e4814e0494aeac676c815f6944892a3348
Peter Fortuna <peter.j.fortuna@gmail.com>
2023-09-08 09:29:13 -0400

    update

---
+++ project-euler/problem-7.py
@@ -0,0 +1,25 @@
+# Problem:
+# Find the 10,001st prime number.
+
+
+# Solution:
+# To find the 10,001st prime, we find each prime from the first to the 
+# 10,000th, and check each integer for divisibility by this set of prime 
+# numbers.
+#
+# Computational complexity: O(n^2)
+# Storage complexity: O(n)
+
+def solution(n=10001):
+    primes = [2]
+    i = 3
+    while len(primes) < n:
+        is_prime = True
+        for prime in primes:
+            if i % prime == 0:
+                is_prime = False
+                break
+        if is_prime:
+            primes.append(i)
+        i += 1
+    return primes[-1]
