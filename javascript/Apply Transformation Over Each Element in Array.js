6ba57707677a0bec57da8367d1ffc42239f91f7c
Peter Fortuna <peter.j.fortuna@gmail.com>
2023-12-04 18:31:53 -0500

    LC JavaScript 30

---
+++ javascript/Apply Transformation Over Each Element in Array.js
@@ -0,0 +1,8 @@
+// Link to problem: https://leetcode.com/problems/apply-transform-over-each-element-in-array
+
+var map = function(arr, fn) {
+    for (let i=0; i < arr.length; i++){
+        arr[i] = fn(arr[i], i);
+    }
+    return arr;
+};
