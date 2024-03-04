6dad4a985fea191415c7710c54a65b1bd091cd19
Peter Fortuna <peter.j.fortuna@gmail.com>
2024-03-04 14:18:44 -0500

    JavaScript 30

---
+++ leetcode/javascript/Event Emitter.js
@@ -0,0 +1,60 @@
+// Link to problem: https://leetcode.com/problems/event-emitter/
+
+class EventEmitter {
+    constructor() {
+        this.events = new Map();
+    }
+
+    /**
+     * @param {string} eventName
+     * @param {Function} callback
+     * @return {Object}
+     */
+    subscribe(eventName, callback) {
+        if (!this.events.has(eventName)){
+            this.events.set(eventName, []);
+        }
+
+        this.events.get(eventName).push(callback);
+
+        return {
+            unsubscribe: () => {
+                const index = this.events.get(eventName).indexOf(callback);
+                if (index !== -1){
+                    this.events.get(eventName).splice(index, 1);
+                }
+            }
+        };
+    }
+    
+    /**
+     * @param {string} eventName
+     * @param {Array} args
+     * @return {Array}
+     */
+    emit(eventName, args = []) {
+        if (!this.events.has(eventName)){
+            return [];
+        }
+        const listeners = this.events.get(eventName);
+        const results = [];
+
+        for (const listener of listeners) {
+            results.push(listener(...args));
+        }
+
+        return results;
+    }
+}
+
+/**
+ * const emitter = new EventEmitter();
+ *
+ * // Subscribe to the onClick event with onClickCallback
+ * function onClickCallback() { return 99 }
+ * const sub = emitter.subscribe('onClick', onClickCallback);
+ *
+ * emitter.emit('onClick'); // [99]
+ * sub.unsubscribe(); // undefined
+ * emitter.emit('onClick'); // []
+ */
